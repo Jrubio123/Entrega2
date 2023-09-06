16a3ed46019b281f107abee7a5a226024661ac5e
Juan Pablo Rubio Mejia <juanpablo.rubio990221@gmail.com>
2023-09-06 11:27:29 -0500

    solucion localStorage ya se pueden visualizar

--- dist/index.8f0c9192.js
+++ dist/index.8f0c9192.js
@@ -92,68 +92,140 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 //js de agendar
+/*
 const barberoSelect = document.getElementById("barbero");
 const fechaInput = document.getElementById("fecha");
 const horaInput = document.getElementById("hora");
 const clienteInput = document.getElementById("cliente");
 const agendarBtn = document.getElementById("agendarBtn");
-agendarBtn.addEventListener("click", function() {
+
+agendarBtn.addEventListener("click", function () {
     const selectedBarbero = barberoSelect.value;
     const fecha = fechaInput.value;
     const hora = horaInput.value;
     const cliente = clienteInput.value;
+
     if (selectedBarbero && fecha && hora && cliente) {
         const mensaje = `¡Hola ${selectedBarbero}! Tienes una cita agendada para el ${fecha} a las ${hora} con el cliente ${cliente}.`;
         const encodedMensaje = encodeURIComponent(mensaje);
+
         const telefonoBarbero = "573126204076";
         const url = `https://wa.me/${telefonoBarbero}/?text=${encodedMensaje}`;
+
         window.location.href = url;
+
         const cita = {
             barbero: selectedBarbero,
             fecha: fecha,
             hora: hora,
             cliente: cliente
         };
+
         localStorage.setItem("cita", JSON.stringify(cita));
-    } else alert("Por favor, completa todos los campos antes de agendar la cita.");
+    } else {
+        alert("Por favor, completa todos los campos antes de agendar la cita.");
+    }
 });
+
+
 //js de reservas
-window.addEventListener("load", ()=>{
-    const reservasContainer = document.getElementById("reservasContainer");
-    const citasAgendadas = JSON.parse(localStorage.getItem("citas")) || [];
-    function mostrarCitasAgendadas() {
-        reservasContainer.innerHTML = ""; // Limpiar el contenido previo
-        citasAgendadas.forEach((cita)=>{
-            const citaElement = document.createElement("div");
-            citaElement.classList.add("cita-item");
-            const barberoElement = document.createElement("p");
-            barberoElement.textContent = `Barbero: ${cita.barbero}`;
-            citaElement.appendChild(barberoElement);
-            const fechaElement = document.createElement("p");
-            fechaElement.textContent = `Fecha: ${cita.fecha}`;
-            citaElement.appendChild(fechaElement);
-            const horaElement = document.createElement("p");
-            horaElement.textContent = `Hora: ${cita.hora}`;
-            citaElement.appendChild(horaElement);
-            const clienteElement = document.createElement("p");
-            clienteElement.textContent = `Cliente: ${cita.cliente}`;
-            citaElement.appendChild(clienteElement);
-            const cancelarBtn = document.createElement("button");
-            cancelarBtn.textContent = "Cancelar Cita";
-            cancelarBtn.addEventListener("click", ()=>cancelarCita(cita));
-            citaElement.appendChild(cancelarBtn);
-            reservasContainer.appendChild(citaElement);
+
+// Obtener el contenedor de reservas
+const reservasContainer = document.getElementById("reservasContainer");
+
+// Función para mostrar las reservas
+function mostrarReservas() {
+    // Obtener las reservas almacenadas en localStorage
+    const reservas = JSON.parse(localStorage.getItem("cita")) || [];
+
+    // Limpiar el contenido actual del contenedor
+    reservasContainer.innerHTML = "";
+
+    // Verificar si hay reservas para mostrar
+    if (reservas.length === 0) {
+        reservasContainer.innerHTML = "<p>No hay reservas agendadas.</p>";
+
+    } else {
+        // Crear una lista de reservas
+        const listaReservas = document.createElement("ul");
+
+        // Iterar a través de las reservas y agregarlas a la lista
+        reservas.forEach((reserva, index) => {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `<strong>Reserva ${index + 1}:</strong> Barbero: ${reserva.barbero}, Fecha: ${reserva.fecha}, Hora: ${reserva.hora}, Cliente: ${reserva.cliente}`;
+            listaReservas.appendChild(listItem);
         });
+        // Agregar la lista de reservas al contenedor
+        reservasContainer.appendChild(listaReservas);
     }
-    function cancelarCita(cita) {
-        const index = citasAgendadas.indexOf(cita);
-        if (index !== -1) {
-            citasAgendadas.splice(index, 1);
-            localStorage.setItem("citas", JSON.stringify(citasAgendadas));
-            mostrarCitasAgendadas();
-        }
+}
+// Llamar a la función para mostrar las reservas al cargar la página
+mostrarReservas();
+*/ // Obtener el contenedor de reservas
+const reservasContainer = document.getElementById("reservasContainer");
+// Función para mostrar las reservas
+function mostrarReservas() {
+    // Obtener las reservas almacenadas en localStorage
+    const reservas = JSON.parse(localStorage.getItem("reservas")) || [];
+    // Limpiar el contenido actual del contenedor
+    reservasContainer.innerHTML = "";
+    // Verificar si hay reservas para mostrar
+    if (reservas.length === 0) reservasContainer.innerHTML = "<p>No hay reservas agendadas.</p>";
+    else {
+        // Crear una lista de reservas
+        const listaReservas = document.createElement("ul");
+        // Iterar a través de las reservas y agregarlas a la lista
+        reservas.forEach((reserva, index)=>{
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `<strong>Reserva ${index + 1}:</strong> Barbero: ${reserva.barbero}, Fecha: ${reserva.fecha}, Hora: ${reserva.hora}, Cliente: ${reserva.cliente}`;
+            listaReservas.appendChild(listItem);
+        });
+        // Agregar la lista de reservas al contenedor
+        reservasContainer.appendChild(listaReservas);
     }
-    mostrarCitasAgendadas();
+}
+// Llamar a la función para mostrar las reservas al cargar la página
+mostrarReservas();
+// Obtener elementos del formulario de agendar
+const barberoSelect = document.getElementById("barbero");
+const fechaInput = document.getElementById("fecha");
+const horaInput = document.getElementById("hora");
+const clienteInput = document.getElementById("cliente");
+const agendarBtn = document.getElementById("agendarBtn");
+// Manejar el evento de clic en el botón de agendar
+agendarBtn.addEventListener("click", function() {
+    const selectedBarbero = barberoSelect.value;
+    const fecha = fechaInput.value;
+    const hora = horaInput.value;
+    const cliente = clienteInput.value;
+    if (selectedBarbero && fecha && hora && cliente) {
+        // Obtener las reservas actuales desde localStorage
+        const reservas = JSON.parse(localStorage.getItem("reservas")) || [];
+        const mensaje = `¡Hola ${selectedBarbero}! Tienes una cita agendada para el ${fecha} a las ${hora} con el cliente ${cliente}.`;
+        const encodedMensaje = encodeURIComponent(mensaje);
+        const telefonoBarbero = "573126204076";
+        const url = `https://wa.me/${telefonoBarbero}/?text=${encodedMensaje}`;
+        window.location.href = url;
+        // Crear un objeto de reserva
+        const cita = {
+            barbero: selectedBarbero,
+            fecha: fecha,
+            hora: hora,
+            cliente: cliente
+        };
+        // Agregar la reserva al arreglo de reservas
+        reservas.push(cita);
+        // Guardar el arreglo actualizado en localStorage
+        localStorage.setItem("reservas", JSON.stringify(reservas));
+        // Llamar a la función para mostrar las reservas actualizadas
+        mostrarReservas();
+        // Restablecer los campos del formulario
+        barberoSelect.value = "";
+        fechaInput.value = "";
+        horaInput.value = "";
+        clienteInput.value = "";
+        alert("Cita agendada con \xe9xito.");
+    } else alert("Por favor, completa todos los campos antes de agendar la cita.");
 });
 
 //# sourceMappingURL=index.8f0c9192.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 //js de agendar
-
+/*
 const barberoSelect = document.getElementById("barbero");
 const fechaInput = document.getElementById("fecha");
 const horaInput = document.getElementById("hora");
@@ -155,50 +155,125 @@ agendarBtn.addEventListener("click", function () {
 
 //js de reservas
 
-window.addEventListener("load", () => {
-    const reservasContainer = document.getElementById("reservasContainer");
-    const citasAgendadas = JSON.parse(localStorage.getItem("citas")) || [];
+// Obtener el contenedor de reservas
+const reservasContainer = document.getElementById("reservasContainer");
 
-    function mostrarCitasAgendadas() {
-        reservasContainer.innerHTML = ""; // Limpiar el contenido previo
+// Función para mostrar las reservas
+function mostrarReservas() {
+    // Obtener las reservas almacenadas en localStorage
+    const reservas = JSON.parse(localStorage.getItem("cita")) || [];
 
-        citasAgendadas.forEach(cita => {
-            const citaElement = document.createElement("div");
-            citaElement.classList.add("cita-item");
+    // Limpiar el contenido actual del contenedor
+    reservasContainer.innerHTML = "";
 
-            const barberoElement = document.createElement("p");
-            barberoElement.textContent = `Barbero: ${cita.barbero}`;
-            citaElement.appendChild(barberoElement);
+    // Verificar si hay reservas para mostrar
+    if (reservas.length === 0) {
+        reservasContainer.innerHTML = "<p>No hay reservas agendadas.</p>";
 
-            const fechaElement = document.createElement("p");
-            fechaElement.textContent = `Fecha: ${cita.fecha}`;
-            citaElement.appendChild(fechaElement);
+    } else {
+        // Crear una lista de reservas
+        const listaReservas = document.createElement("ul");
 
-            const horaElement = document.createElement("p");
-            horaElement.textContent = `Hora: ${cita.hora}`;
-            citaElement.appendChild(horaElement);
-
-            const clienteElement = document.createElement("p");
-            clienteElement.textContent = `Cliente: ${cita.cliente}`;
-            citaElement.appendChild(clienteElement);
-
-            const cancelarBtn = document.createElement("button");
-            cancelarBtn.textContent = "Cancelar Cita";
-            cancelarBtn.addEventListener("click", () => cancelarCita(cita));
-            citaElement.appendChild(cancelarBtn);
-
-            reservasContainer.appendChild(citaElement);
+        // Iterar a través de las reservas y agregarlas a la lista
+        reservas.forEach((reserva, index) => {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `<strong>Reserva ${index + 1}:</strong> Barbero: ${reserva.barbero}, Fecha: ${reserva.fecha}, Hora: ${reserva.hora}, Cliente: ${reserva.cliente}`;
+            listaReservas.appendChild(listItem);
         });
+        // Agregar la lista de reservas al contenedor
+        reservasContainer.appendChild(listaReservas);
     }
+}
+// Llamar a la función para mostrar las reservas al cargar la página
+mostrarReservas();
+*/
 
-    function cancelarCita(cita) {
-        const index = citasAgendadas.indexOf(cita);
-        if (index !== -1) {
-            citasAgendadas.splice(index, 1);
-            localStorage.setItem("citas", JSON.stringify(citasAgendadas));
-            mostrarCitasAgendadas();
-        }
+// Obtener el contenedor de reservas
+const reservasContainer = document.getElementById("reservasContainer");
+
+// Función para mostrar las reservas
+function mostrarReservas() {
+    // Obtener las reservas almacenadas en localStorage
+    const reservas = JSON.parse(localStorage.getItem("reservas")) || [];
+
+    // Limpiar el contenido actual del contenedor
+    reservasContainer.innerHTML = "";
+
+    // Verificar si hay reservas para mostrar
+    if (reservas.length === 0) {
+        reservasContainer.innerHTML = "<p>No hay reservas agendadas.</p>";
+    } else {
+        // Crear una lista de reservas
+        const listaReservas = document.createElement("ul");
+
+        // Iterar a través de las reservas y agregarlas a la lista
+        reservas.forEach((reserva, index) => {
+            const listItem = document.createElement("li");
+            listItem.innerHTML = `<strong>Reserva ${index + 1}:</strong> Barbero: ${reserva.barbero}, Fecha: ${reserva.fecha}, Hora: ${reserva.hora}, Cliente: ${reserva.cliente}`;
+            listaReservas.appendChild(listItem);
+        });
+
+        // Agregar la lista de reservas al contenedor
+        reservasContainer.appendChild(listaReservas);
     }
+}
 
-    mostrarCitasAgendadas();
+// Llamar a la función para mostrar las reservas al cargar la página
+mostrarReservas();
+
+// Obtener elementos del formulario de agendar
+const barberoSelect = document.getElementById("barbero");
+const fechaInput = document.getElementById("fecha");
+const horaInput = document.getElementById("hora");
+const clienteInput = document.getElementById("cliente");
+const agendarBtn = document.getElementById("agendarBtn");
+
+// Manejar el evento de clic en el botón de agendar
+agendarBtn.addEventListener("click", function () {
+    const selectedBarbero = barberoSelect.value;
+    const fecha = fechaInput.value;
+    const hora = horaInput.value;
+    const cliente = clienteInput.value;
+
+    if (selectedBarbero && fecha && hora && cliente) {
+        // Obtener las reservas actuales desde localStorage
+        const reservas = JSON.parse(localStorage.getItem("reservas")) || [];
+
+        
+        const mensaje = `¡Hola ${selectedBarbero}! Tienes una cita agendada para el ${fecha} a las ${hora} con el cliente ${cliente}.`;
+        const encodedMensaje = encodeURIComponent(mensaje);
+
+        const telefonoBarbero = "573126204076";
+        const url = `https://wa.me/${telefonoBarbero}/?text=${encodedMensaje}`;
+
+        window.location.href = url;
+
+
+        // Crear un objeto de reserva
+        const cita = {
+            barbero: selectedBarbero,
+            fecha: fecha,
+            hora: hora,
+            cliente: cliente
+        };
+
+        // Agregar la reserva al arreglo de reservas
+        reservas.push(cita);
+
+        // Guardar el arreglo actualizado en localStorage
+        localStorage.setItem("reservas", JSON.stringify(reservas));
+
+        // Llamar a la función para mostrar las reservas actualizadas
+        mostrarReservas();
+
+        // Restablecer los campos del formulario
+        barberoSelect.value = "";
+        fechaInput.value = "";
+        horaInput.value = "";
+        clienteInput.value = "";
+
+        alert("Cita agendada con éxito.");
+    } else {
+        alert("Por favor, completa todos los campos antes de agendar la cita.");
+    }
 });
